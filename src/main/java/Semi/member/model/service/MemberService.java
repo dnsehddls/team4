f98b71f25fab6cc30ca2e4858cd0187d9fca025c
mothership <mothership2002@naver.com>
2022-06-09 19:02:55 +0900

    메인페이지 관련 작업 메인 병합 (임동현)

--- src/main/java/Semi/member/model/service/MemberService.java
+++ src/main/java/Semi/member/model/service/MemberService.java
@@ -1,5 +1,22 @@
 package Semi.member.model.service;
 
-public class MemberService {
+import static Semi.common.JDBCTemplate.*;
 
+import java.sql.Connection;
+
+import Semi.member.model.dao.MemberDAO;
+import Semi.member.model.vo.Member;
+
+public class MemberService {
+	
+	private MemberDAO dao = new MemberDAO();
+
+	public Member login(Member user) throws Exception{
+		Connection conn = getConnection();
+		Member result = dao.login(conn,user);
+		close(conn);
+		return result;
+	}
+	
+	
 }
--- src/main/java/Semi/member/model/vo/Member.java
+++ src/main/java/Semi/member/model/vo/Member.java
@@ -1,5 +1,26 @@
 package Semi.member.model.vo;
 
-public class Member {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Member {
+	private int memberNo;
+	private String memberID;
+	private String memberPW;
+	private String memberName;
+	private String memberNickname;
+	private String memberEmail;
+	private String memberTel;
+	private String memberProfile;
+	private int point;
+	private String grade;
+	private String registDate;
+	private String secession_Flag;
+	
 }
